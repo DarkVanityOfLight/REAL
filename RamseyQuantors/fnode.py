b21626699efbd24c5a33dda1ebe97cb37ca47ba1
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2025-05-05 11:03:33 +0200

    chore: Remove wrong typehint

--- RamseyQuantors/fnode.py
+++ RamseyQuantors/fnode.py
@@ -20,7 +20,7 @@ class ExtendedFNode(FNode):
         """Return True if this node is any quantifier (exists, forall, or Ramsey)."""
         return self.is_exists() or self.is_forall() or self.is_ramsey()
 
-    def quantifier_vars(self) -> Tuple[Self, ...]:
+    def quantifier_vars(self):
         """
         Return a tuple of bound variables for this quantifier.
 
