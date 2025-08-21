465872660f8e04d00a349e1ab21c51de52943db2
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2025-08-21 11:55:31 +0200

    chore: Reset env after each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,12 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from pysmt.environment import pop_env
 import pytest
 from ramsey_extensions.environment import push_ramsey
 
 
-@pytest.fixture(scope="session", autouse=True)
-def push_ramsey_once():
+@pytest.fixture(autouse=True)
+def cleanup():
+    pop_env()
     push_ramsey()
