37240592f67981437fab849222743944af2347ae
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2025-08-22 12:39:55 +0200

    fix: Fix the ramsey substituter

--- ramsey_extensions/substituter.py
+++ ramsey_extensions/substituter.py
@@ -1,3 +1,4 @@
+import pysmt.walkers
 from pysmt.substituter import MGSubstituter
 
 class ExtendedMGSubstituter(MGSubstituter):
@@ -14,4 +15,16 @@ class ExtendedMGSubstituter(MGSubstituter):
         res = substitutions.get(formula, None)
         if res is None:
             res = self.mgr.ToInt(args[0])
+        return res
+
+    def walk_ramsey(self, formula, args, **kwargs):
+        substitutions = kwargs['substitutions']
+        res = substitutions.get(formula, None)
+        if res is None:
+            qvars = ([pysmt.walkers.IdentityDagWalker.walk_symbol(self, v, args, **kwargs)
+                for v in formula.quantifier_vars()[0]],
+                     [pysmt.walkers.IdentityDagWalker.walk_symbol(self, v, args, **kwargs)
+                for v in formula.quantifier_vars()[1]])
+
+            res = self.mgr.Ramsey(*qvars, args[0])
         return res
