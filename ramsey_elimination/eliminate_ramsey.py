5b7481aa77966fc3887936baadbeddaf560f3acf
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2025-08-25 12:04:12 +0200

    feat: New type detection

--- ramsey_elimination/eliminate_ramsey.py
+++ ramsey_elimination/eliminate_ramsey.py
@@ -1,17 +1,27 @@
-from pysmt.typing import INT
-from ramsey_elimination.integer_elimination import eliminate_ramsey_int
-from ramsey_elimination.mixed_elimination import eliminate_ramsey_mixed
-from ramsey_elimination.real_elimination import eliminate_ramsey_real
+from enum import Enum, auto
+from ramsey_elimination.integer_elimination import eliminate_ramsey_int, full_ramsey_elimination_int
+from ramsey_elimination.mixed_elimination import eliminate_ramsey_mixed, full_mixed_ramsey_elimination
+from ramsey_elimination.real_elimination import eliminate_ramsey_real, full_ramsey_elimination_real
 from ramsey_extensions.fnode import ExtendedFNode
 
+
+class _RamseyTypes(Enum):
+    INT = auto()
+    REAL = auto()
+    MIXED = auto()
 
 def eliminate_ramsey(f: ExtendedFNode) -> ExtendedFNode:
     assert f.is_ramsey()
 
-    if f.arg(0).is_ramsey():
-        return eliminate_ramsey_mixed(f)
-    elif f.quantifier_vars()[0][0].symbol_type() == INT:
-        return eliminate_ramsey_int(f)
-    else:
-        return eliminate_ramsey_real(f)
+    q1, _ = f.quantifier_vars()
+    if all(v.symbol_type().is_real_type() for v in q1): typ = _RamseyTypes.REAL
+    elif all(v.symbol_type().is_int_type() for v in q1): typ = _RamseyTypes.INT
+    else: typ = _RamseyTypes.MIXED
 
+    match typ:
+        case _RamseyTypes.INT:
+            return full_ramsey_elimination_int(f)
+        case _RamseyTypes.REAL:
+            return full_ramsey_elimination_real(f)
+        case _RamseyTypes.MIXED:
+            return full_mixed_ramsey_elimination(f)
